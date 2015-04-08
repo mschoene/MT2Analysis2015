c0ca0111a286e5eb8c0db785d19a44a32fa57df5
Bruno <casal@cern.ch>
2015-04-08 14:49:13 +0200

    first version of MT2EstimateQCD class

---
+++ interface/MT2EstimateQCD.h
@@ -0,0 +1,82 @@
+#ifndef MT2EstimateQCD_h
+#define MT2EstimateQCD_h
+
+#include "MT2Estimate.h"
+
+#include <iostream>
+#include <vector>
+
+
+class MT2EstimateQCD : public MT2Estimate {
+
+ public:
+
+  MT2EstimateQCD( const MT2EstimateQCD& rhs );
+  MT2EstimateQCD( const std::string& aname, const MT2Region& aregion );
+  virtual ~MT2EstimateQCD();
+
+  virtual void setName( const std::string& newName );
+ 
+  
+  TH1D* lDphi;
+  TH1D* hDphi;
+  TH1D* ratio;
+
+  TF1* exp;
+  TF1* expPlusC;
+  TF1* expOrC;
+
+  const MT2EstimateQCD& operator=( const MT2EstimateQCD& rhs );
+  MT2EstimateQCD operator+( const MT2EstimateQCD& rhs ) const;
+  MT2EstimateQCD operator-( const MT2EstimateQCD& rhs ) const;
+  //MT2EstimateQCD operator/( const MT2EstimateQCD& rhs ) const;
+  const MT2EstimateQCD& operator+=( const MT2EstimateQCD& rhs );
+  const MT2EstimateQCD& operator-=( const MT2EstimateQCD& rhs );
+  //MT2EstimateQCD operator/=( const MT2EstimateQCD& rhs ) const;
+
+  MT2EstimateQCD operator* ( float k ) const;
+  MT2EstimateQCD operator/ ( float k ) const;
+  const MT2EstimateQCD& operator*=( float k );
+  const MT2EstimateQCD& operator/=( float k );
+
+  friend MT2EstimateQCD operator*( float k, const MT2EstimateQCD& rhs );
+  friend MT2EstimateQCD operator/( float k, const MT2EstimateQCD& rhs );
+
+  void fillDphi( float dphi, float weight=1., float mt2=-1 );
+
+  void doFit();
+
+  void getRatio() {
+    ratio.Divide(hDphi, lDphi)
+  }
+
+  void setFitXmin(float xmin){
+    fitXmin = xmin;
+  }
+  void setFitXmax(float xmax){
+    fitXmaz = xmax;
+  }
+  void setDphiLow(float dphi){
+    dphi_low = dphi;
+  }
+
+  virtual void finalize();
+
+  virtual void getShit( TFile* file, const std::string& path );
+
+  virtual void write() const;
+
+  virtual void print(const std::string& ofs);
+
+ private:
+
+  float fitXmin, fitXmax;  // fit window
+  float dphi_low;          // threshold to define low dphi region
+
+};
+
+
+
+
+
+#endif
