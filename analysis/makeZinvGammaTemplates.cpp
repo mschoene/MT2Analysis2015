cff2cf9da426632876a0512d3ee405907c6046b7
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2015-07-10 16:46:00 +0200

    added possibility to override cfg

--- analysis/makeZinvGammaTemplates.cpp
+++ analysis/makeZinvGammaTemplates.cpp
@@ -44,7 +44,7 @@ int main( int argc, char* argv[] ) {
 
 
  
-  if( argc!=2 ) {
+  if( argc<2 ) {
     std::cout << "USAGE: ./makeZinvGammaTemplates [configFileName]" << std::endl;
     std::cout << "Exiting." << std::endl;
     exit(11);
