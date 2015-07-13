e4ae1910ef00a20c6300e109564412bad40ae0e9
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2015-07-13 15:50:20 +0200

    default is to overwrite

--- interface/MT2Analysis.h
+++ interface/MT2Analysis.h
@@ -79,7 +79,7 @@ class MT2Analysis {
 
   static MT2Analysis* readFromFile( const std::string& fileName, const std::string& matchName="" );
   static std::vector<MT2Analysis*> readAllFromFile( const std::string& fileName, const std::string& matchName="", bool verbose=true );
-  void writeToFile( const std::string& fileName, const std::string& option="RECREATE", bool overwrite=false );
+  void writeToFile( const std::string& fileName, const std::string& option="UPDATE", bool overwrite=true );
   void addToFile( const std::string& fileName, bool overwrite=false ) {
     return this->writeToFile(fileName,"UPDATE",overwrite);
   }
