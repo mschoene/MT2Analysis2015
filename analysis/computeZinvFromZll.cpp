b5b6948d615e7a3310c52be3ece4af1e16006097
mmasciov <mario.masciovecchio@cern.ch>
2015-07-31 14:53:34 +0200

    Cleaned zllControlRegion and computeZinvFromZll (integral)

--- analysis/computeZinvFromZll.cpp
+++ analysis/computeZinvFromZll.cpp
@@ -9,6 +9,7 @@
 #include "../interface/MT2Analysis.h"
 #include "../interface/MT2EstimateZinvGamma.h"
 #include "../interface/MT2EstimateTree.h"
+#include "../interface/MT2EstimateSyst.h"
 #include "../interface/MT2Region.h"
 #include "../interface/MT2Sample.h"
 #include "../interface/MT2Config.h"
@@ -27,141 +28,86 @@
 
 
 
-
-
-//float lumi = 0.1;
-float lumi = 20.;
-
 MT2Analysis<MT2Estimate>* combineDataAndMC( MT2Analysis<MT2Estimate>* data, MT2Analysis<MT2Estimate>* mc );
-
-
 MT2Analysis<MT2Estimate>* getInclusiveRatioMC( const std::string& regionsSet, MT2Analysis<MT2EstimateTree>* Zinv, MT2Analysis<MT2EstimateTree>* gammaCRtree );
 
 
 
 int main(int argc, char* argv[]) {
 
-
-   std::string regionsSet = "zurich";
-  //std::string regionsSet = "13TeV_inclusive";
- if( argc>2 ) {
-    regionsSet = std::string(argv[2]);
-  }
-
-  std::cout << "-> Using regions: " << regionsSet << std::endl;
-
   
-  if( argc<2 ) {
-    std::cout << "USAGE: ./regionEventYields [configFileName] regionSet" << std::endl;
+  std::cout << "------------------------------------------------------" << std::endl;
+  std::cout << "|                                                    |" << std::endl;
+  std::cout << "|                                                    |" << std::endl;
+  std::cout << "|           Running computeZinvFromGamma             |" << std::endl;
+  std::cout << "|                                                    |" << std::endl;
+  std::cout << "|                                                    |" << std::endl;
+  std::cout << "------------------------------------------------------" << std::endl;
+  std::cout << std::endl << std::endl;
+
+
+  if( argc!=2 ) {
+    std::cout << "USAGE: ./computeZinvFromGamma [configFileName]" << std::endl;
     std::cout << "Exiting." << std::endl;
     exit(11);
   }
-  
+
 
   std::string configFileName(argv[1]);
-
-
-  MT2Config cfg("cfgs/" + configFileName + ".txt");
+  MT2Config cfg(configFileName);
 
 
   TH1::AddDirectory(kFALSE); // stupid ROOT memory allocation needs this
 
-  std::string samplesFileName = "../samples/samples_" + cfg.mcSamples() + ".dat";
-  //  std::string samplesFileName = "../samples/samples_samples_PHYS14_skimprune.dat";
+  std::string zllControlRegionDir = cfg.getEventYieldDir() + "/zllControlRegion";
 
-  std::cout << std::endl << std::endl;
-  std::cout << "-> Loading samples from file: " << samplesFileName << std::endl;
+  MT2Analysis<MT2Estimate>* zllCR = MT2Analysis<MT2Estimate>::readFromFile(zllControlRegionDir + "/data.root", "zllCR");
+  MT2Analysis<MT2Estimate>* zllCR_mc = MT2Analysis<MT2Estimate>::readFromFile(zllControlRegionDir + "/mc.root", "zllCR");                                                                     
 
+  MT2Analysis<MT2Estimate>* zllCR_integral = MT2Estimate::makeIntegralAnalysisFromEstimate( "zllCR_integral", cfg.regionsSet(), zllCR );
+  MT2Analysis<MT2Estimate>* zllCR_mc_integral = MT2Estimate::makeIntegralAnalysisFromEstimate( "zllCR_mc_integral", cfg.regionsSet(), zllCR_mc );
 
+  if( zllCR==0 || zllCR_mc==0 ) {
+    std::cout << "-> Please run zllControlRegion first. I need to get the zllCR yields from there." << std::endl;
+    std::cout << "-> Thank you for your cooperation." << std::endl;
+    exit(193);
+  }
 
-  std::string outputdir = "Zll_CR_" + cfg.mcSamples() + "_" + regionsSet;
-  //  std::string outputdir = "Zll_" + configFileName;
-    double intpart;
-    double fracpart = modf(lumi, &intpart);
-    std::string suffix;
-    if( fracpart>0. )
-      suffix = std::string( Form("_%.0fp%.0ffb", intpart, 10.*fracpart ) );
-    else
-      suffix = std::string( Form("_%.0ffb", intpart ) );
-    outputdir += suffix;
-  
-  system(Form("mkdir -p %s", outputdir.c_str()));
- 
-
-  MT2Analysis<MT2Estimate>* Zll_data = MT2Analysis<MT2Estimate>::readFromFile(Form("%s/data.root",outputdir.c_str()), "Zll");
-
-  MT2Analysis<MT2Estimate>* Zll_mc = MT2Analysis<MT2Estimate>::readFromFile(Form("%s/mc.root",outputdir.c_str()), "Zll");
-
-
-
-  MT2Analysis<MT2EstimateTree>* Zinv = MT2Analysis<MT2EstimateTree>::readFromFile(Form("EventYields_mc_PHYS14_v5_dummy_%.0ffb/analyses.root", lumi), "ZJets");
+  MT2Analysis<MT2EstimateTree>* Zinv = MT2Analysis<MT2EstimateTree>::readFromFile(cfg.getEventYieldDir() + "/analyses.root", "ZJets");
   if( Zinv==0 ) {
     std::cout << "-> Please run regionEventYields on MC first. I need to get the Z->vv MC yields from there." << std::endl;
     std::cout << "-> Thank you for your cooperation." << std::endl;
     exit(197);
   }
 
+  MT2Analysis<MT2Estimate>* ZZllRatioMC = new MT2Analysis<MT2Estimate>( "ZZllRatioMC", cfg.regionsSet() );
+  //(*ZZllRatioMC) = ( (* (MT2Analysis<MT2Estimate>*)Zinv) / (*zllCR_mc) );
+  (*ZZllRatioMC) = ( (* (MT2Analysis<MT2Estimate>*)Zinv) / (*zllCR_mc_integral) );
+
+  MT2Analysis<MT2Estimate>* ZZllRatio = new MT2Analysis<MT2Estimate>( "ZZllRatio", cfg.regionsSet() );
+  (*ZZllRatio) = (*ZZllRatioMC);
+
+  MT2Analysis<MT2Estimate>* ZinvEstimateFromZll = new MT2Analysis<MT2Estimate>( "ZinvEstimateFromZll", cfg.regionsSet() );
+  //(*ZinvEstimateFromZll) = (*zllCR) * (*ZZllRatio);
+  (*ZinvEstimateFromZll) = (*zllCR_integral)*(*ZZllRatio);
 
 
-
- 
-  MT2Analysis<MT2Estimate>* ZllRatioMC = new MT2Analysis<MT2Estimate>( "ZllRatioMC", regionsSet );
-  (*ZllRatioMC) = ( (* (MT2Analysis<MT2Estimate>*)Zinv) / (*Zll_mc) );
-  
-
-
-  MT2Analysis<MT2Estimate>* ZllRatio = new MT2Analysis<MT2Estimate>( "ZllRatio", regionsSet );
-  (*ZllRatio) = (*ZllRatioMC);
-
-
-  //"Data" estimate from the rounded data times the mc ratio of the SR/CR
-  MT2Analysis<MT2Estimate>* ZinvEstimateFromZll = new MT2Analysis<MT2Estimate>( "ZinvEstimateFromZll", regionsSet );
-  (*ZinvEstimateFromZll) = (*Zll_data) * (*ZllRatio);
-
+  //MT2Analysis<MT2EstimateSyst>* ZinvEstimateFromGamma = MT2EstimateSyst::makeAnalysisFromEstimate( "ZinvEstimateFromGamma", regionsSet, gammaCR_times_ZgammaRatio );
 
   MT2Analysis<MT2Estimate>* ZinvEstimate = combineDataAndMC( ZinvEstimateFromZll, (MT2Analysis<MT2Estimate>*)Zinv );
 
+  std::string outFile = cfg.getEventYieldDir() + "/zinvFromZll";
+  outFile += ".root";
 
-
-  std::string outFile = outputdir + "/MT2ZinvEstimate.root";
-
-
-  ZinvEstimate->writeToFile( outFile );
-  ZllRatio->addToFile( outFile );
+  ZinvEstimate->writeToFile( outFile, "recreate" );
+  ZZllRatio->addToFile( outFile );
   Zinv->setName("Zinv");
-
   Zinv->addToFile( outFile );
-
-  Zll_data->addToFile( outFile );
-
- 
-
-
-
-
+  zllCR->addToFile( outFile );
 
   return 0;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
