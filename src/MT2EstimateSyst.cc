179464cb57d854292107522d380e1892f702d487
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2015-10-02 11:25:52 +0200

    removed addOverflow

--- src/MT2EstimateSyst.cc
+++ src/MT2EstimateSyst.cc
@@ -212,22 +212,6 @@ void MT2EstimateSyst::setName( const std::string& newName ) {
 
 
 
-void MT2EstimateSyst::finalize() {
-
-  this->addOverflow();
-
-}
-
-
-void MT2EstimateSyst::addOverflow() {
-
-  MT2Estimate::addOverflow();
-
-  MT2Estimate::addOverflowSingleHisto( yield_systUp );
-  MT2Estimate::addOverflowSingleHisto( yield_systDown );
-
-}
-
 
 
 void MT2EstimateSyst::getShit( TFile* file, const std::string& path ) {
