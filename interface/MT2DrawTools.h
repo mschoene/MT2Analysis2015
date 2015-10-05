7adebbc89d0236ea477e5d3a562ca1bcedef8a0d
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2015-10-05 13:12:59 +0200

    added topoLabel

--- interface/MT2DrawTools.h
+++ interface/MT2DrawTools.h
@@ -83,7 +83,7 @@ class MT2DrawTools {
   static void addOverflowSingleHisto( TH3D* yield3d );
 
 
-  void drawRegionYields_fromTree( const std::string& saveName, const std::string& varName, const std::string& selection, int nBins, float xMin, float xMax, std::string axisName="", const std::string& units="", const std::string& cutsLabel="" );
+  void drawRegionYields_fromTree( const std::string& saveName, const std::string& varName, const std::string& selection, int nBins, float xMin, float xMax, std::string axisName="", const std::string& units="", const std::string& kinCuts="", const std::string& topoCuts="" );
   //void drawRegionYields_fromTree( MT2Analysis<MT2EstimateTree>* data, std::vector<MT2Analysis<MT2EstimateTree>* >  bgYields, const std::string& saveName, const std::string& varName, const std::string& selection, int nBins, float xMin, float xMax, std::string axisName="", const std::string& units="", const std::string& cutsLabel="" );
 
  private:
