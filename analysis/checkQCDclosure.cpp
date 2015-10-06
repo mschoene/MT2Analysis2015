55c3f4b1cc5c0c1f8cb1d473e02163b833d486b8
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2015-10-06 15:22:03 +0200

    now plotting nice plots

--- analysis/checkQCDclosure.cpp
+++ analysis/checkQCDclosure.cpp
@@ -58,7 +58,11 @@ int main( int argc, char* argv[] ) {
 
 
   std::string qcdCRdir = cfg.getEventYieldDir() + "/qcdControlRegion";
-  MT2Analysis<MT2EstimateQCD>* qcd = MT2Analysis<MT2EstimateQCD>::readFromFile( qcdCRdir + "/mcFits.root", "mc" );
+  MT2Analysis<MT2EstimateQCD>* qcd = MT2Analysis<MT2EstimateQCD>::readFromFile( qcdCRdir + "/mcFits.root", "qcdOnly" );
+  MT2Analysis<MT2EstimateQCD>* all = MT2Analysis<MT2EstimateQCD>::readFromFile( qcdCRdir + "/mcFits.root", "mc" );
+
+  std::string fitsdir = qcdCRdir + "/fits";
+  system( Form("mkdir -p %s", fitsdir.c_str()) );
 
   std::set<MT2Region> regions = qcd->getRegions();
 
@@ -71,9 +75,12 @@ int main( int argc, char* argv[] ) {
   for( std::set<MT2Region>::iterator iR=regions.begin(); iR!=regions.end(); ++iR ) {
 
     MT2EstimateQCD* thisQCD = qcd->get( *iR );
+    MT2EstimateQCD* thisAll = all->get( *iR );
    
-    TH1D* thisRatio = thisQCD->ratio;    
-    TF1* thisFit = thisQCD->exp;
+    TH1D* thisRatio = thisAll->ratio;    
+    TH1D* thisRatioQCD = thisQCD->ratio;    
+    TF1* thisFit = thisAll->exp;
+    //TF1* thisFit = thisQCD->exp; // this should be true closure but doesn't work!
 
     Double_t expPar = thisFit->GetParameter(0);
     Double_t expParErr = thisFit->GetParError(0);
@@ -109,13 +116,95 @@ int main( int argc, char* argv[] ) {
 
     h1_pull->Fill( zTest );
 
+    
+    TCanvas* c1 = new TCanvas( "c2", "", 600, 600 );
+    c1->cd();
+    c1->SetLogx();
+    c1->SetLogy();
+
+
+    float yMax = thisRatio->GetMaximum()*5.;
+    float yMinAll = thisRatio->GetMinimum()/2.;
+    float yMin = thisRatioQCD->GetMinimum()/2.;
+    if( yMin < 0.03 ) yMin = 0.03;
+    if( yMin > yMinAll ) yMin = yMinAll;
+
+    TH2D* h2_axes = new TH2D("axes", "", 10, 30., xMax, 10, yMin, yMax );
+    h2_axes->SetXTitle( "M_{T2} [GeV]" );
+    h2_axes->SetYTitle( "Events");
+    h2_axes->GetXaxis()->SetNoExponent();
+    h2_axes->GetXaxis()->SetMoreLogLabels();
+    h2_axes->GetYaxis()->SetNoExponent();
+    //h2_axes->GetYaxis()->SetMoreLogLabels();
+    h2_axes->Draw();
+
+    std::vector<std::string> regionNiceNames = iR->getNiceNames();
+
+    TPaveText* regionName = new TPaveText( 0.52, 0.81, 0.9, 0.9, "brNDC" );
+    regionName->SetTextAlign( 11 );
+    regionName->SetTextSize( 0.035 );
+    regionName->SetFillColor( 0 );
+    regionName->AddText( regionNiceNames[0].c_str() );
+    regionName->AddText( regionNiceNames[1].c_str() );
+    regionName->Draw("same");
+
+    TLegend* legend = new TLegend( 0.52, 0.65, 0.9, 0.82 );
+    legend->SetFillColor(0);
+    legend->SetTextSize(0.035);
+    legend->AddEntry( thisRatio, "MC (all)", "P" );
+    legend->AddEntry( thisRatioQCD, "MC (QCD Only)", "P" );
+    legend->AddEntry( thisFit, "Exp. Fit", "L" );
+    legend->Draw("same");
+
+    TLine* lineLeft = new TLine( 60., yMin, 60., yMax );
+    lineLeft->SetLineStyle(2);
+    lineLeft->Draw("same");
+
+    TLine* lineRight = new TLine( 100., yMin, 100., yMax );
+    lineRight->SetLineStyle(2);
+    lineRight->Draw("same");
+
+    thisFit->SetLineColor(46); 
+    thisFit->SetLineWidth(2); 
+    thisFit->Draw("L same");
+ 
+    thisFit_errUp->SetLineWidth(1); 
+    thisFit_errUp->SetLineStyle(2); 
+    thisFit_errUp->SetLineColor(46); 
+    thisFit_errUp->Draw("L same");
+ 
+    thisFit_errDown->SetLineWidth(1); 
+    thisFit_errDown->SetLineStyle(2); 
+    thisFit_errDown->SetLineColor(46); 
+    thisFit_errDown->Draw("L same");
+
+    thisRatio->SetMarkerStyle(20);
+    thisRatio->SetMarkerSize(1.3);
+    thisRatio->Draw("P same");
+ 
+    thisRatioQCD->SetMarkerStyle(24);
+    thisRatioQCD->SetMarkerSize(1.3);
+    thisRatioQCD->Draw("P same");
+ 
+    gPad->RedrawAxis();
+
+    TPaveText* labelTop = MT2DrawTools::getLabelTopSimulation();
+    labelTop->Draw("same");
+
+    c1->SaveAs( Form("%s/ratio_%s.eps", fitsdir.c_str(), iR->getName().c_str()) );
+    c1->SaveAs( Form("%s/ratio_%s.pdf", fitsdir.c_str(), iR->getName().c_str()) );
+
+    delete c1;
+    delete h2_axes;
+
   }
 
   TCanvas* c1 = new TCanvas("c3", "", 600, 600 );
   c1->cd();
   h1_pull->SetXTitle("(MC - fit) / (#sigma_{MC} #oplus #sigma_{fit})");
   h1_pull->Draw();
-  c1->SaveAs("pull.eps");
+  c1->SaveAs( Form("%s/pull.eps", qcdCRdir.c_str()) );
+  c1->SaveAs( Form("%s/pull.pdf", qcdCRdir.c_str()) );
 
   std::cout << h1_pull->GetMean() << " +- " << h1_pull->GetMeanError() << std::endl;
   std::cout << h1_pull->GetRMS()  << " +- " << h1_pull->GetRMSError() << std::endl;
