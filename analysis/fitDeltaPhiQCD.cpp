be9aa21c3798b6c6551ae9a6d586bbca1794fd99
Francesco Pandolfi <francesco.pandolfi@cern.ch>
2015-10-14 12:16:46 +0200

    added drawing, set range to 60-100 expect extremeHT, ratio has now right binning

--- analysis/fitDeltaPhiQCD.cpp
+++ analysis/fitDeltaPhiQCD.cpp
@@ -8,6 +8,8 @@
 #include "TRandom3.h"
 #include "TEfficiency.h"
 #include "TProfile.h"
+#include "TVirtualFitter.h"
+#include "TLegend.h"
 
 
 #include "../interface/MT2Config.h"
@@ -15,6 +17,7 @@
 #include "../interface/MT2Region.h"
 #include "../interface/MT2EstimateQCD.h"
 #include "../interface/MT2EstimateTree.h"
+#include "../interface/MT2DrawTools.h"
 
 
 
@@ -23,6 +26,7 @@
 
 
 void fillFromTreeAndRatio( MT2Estimate* estimate, MT2Estimate* r_effective, TTree* tree, TF1* f1_ratio );
+void drawSingleFit( const std::string& outdir, TF1* thisFitQCD, MT2EstimateQCD* qcd, MT2EstimateQCD* all, float xMin_fit, float xMax_fit );
 
 
 
@@ -45,6 +49,9 @@ int main( int argc, char* argv[] ) {
     std::cout << "Exiting." << std::endl;
     exit(11);
   }
+
+
+  MT2DrawTools::setStyle();
 
 
   std::string configFileName(argv[1]);
@@ -84,6 +91,8 @@ int main( int argc, char* argv[] ) {
 
   std::string qcdCRdir = cfg.getEventYieldDir() + "/qcdControlRegion/";
   std::string outputdir = qcdCRdir;
+  std::string fitsDir = qcdCRdir + "/fits";
+  system( Form("mkdir -p %s", fitsDir.c_str() ));
 
   MT2Analysis<MT2EstimateQCD>* mc_all  = MT2EstimateQCD::makeAnalysisFromInclusiveTree( "mc"     , cfg.qcdRegionsSet(), qcdTree_mc, "" );
   MT2Analysis<MT2EstimateQCD>* qcdOnly = MT2EstimateQCD::makeAnalysisFromInclusiveTree( "qcdOnly", cfg.qcdRegionsSet(), qcdTree_mc, "id>=100 && id<200" );
@@ -93,6 +102,8 @@ int main( int argc, char* argv[] ) {
   std::set<MT2Region> regions = estimate->getRegions();
 
 
+  std::string lastRegionName = "";
+
   for( std::set<MT2Region>::iterator iR = regions.begin(); iR!=regions.end(); ++iR ) {
 
     MT2Estimate* this_estimate     = estimate    ->get( *iR );
@@ -101,10 +112,20 @@ int main( int argc, char* argv[] ) {
     //MT2Estimate* this_f_jets       = f_jets      ->get( *iR );
 
     MT2Region* matchedRegion = qcdOnly->matchRegion(*iR);
-    MT2EstimateQCD* matchedQCDEstimate = qcdOnly->get( *matchedRegion );
-    TF1* f1_ratio = matchedQCDEstimate->getFit( "pow", 70., 100. );
+    MT2EstimateQCD* matchedEstimate_all = mc_all->get( *matchedRegion );
+    MT2EstimateQCD* matchedEstimate_qcd = qcdOnly->get( *matchedRegion );
 
-    fillFromTreeAndRatio( this_estimate, this_r_effective, matchedQCDEstimate->tree, f1_ratio );
+
+    float xMin_fit = (iR->htMin()==1500.) ? 70. : 60.;
+    float xMax_fit = 100.;
+    TF1* f1_ratio = matchedEstimate_qcd->getFit( "pow", xMin_fit, xMax_fit );
+
+    if( matchedRegion->getName()!=lastRegionName ) // draw only one per HT region
+      drawSingleFit( fitsDir, f1_ratio, matchedEstimate_qcd, matchedEstimate_all, xMin_fit, xMax_fit );
+
+    lastRegionName = matchedRegion->getName();
+
+    fillFromTreeAndRatio( this_estimate, this_r_effective, matchedEstimate_qcd->tree, f1_ratio );
 
   }  // for regions
       
@@ -128,11 +149,11 @@ int main( int argc, char* argv[] ) {
 
 void fillFromTreeAndRatio( MT2Estimate* estimate, MT2Estimate* r_effective, TTree* tree, TF1* f1_ratio ) {
 
-  int nBins = estimate->yield->GetNbinsX();
-  float xMin = estimate->yield->GetXaxis()->GetXmin();
-  float xMax = estimate->yield->GetXaxis()->GetXmax();
+  int nBins;
+  double* bins;
+  estimate->region->getBins(nBins, bins);
 
-  TProfile* rprof = new TProfile( "rprof", "", nBins, xMin, xMax );
+  TProfile* rprof = new TProfile( "rprof", "", nBins, bins );
 
   float weight;
   tree->SetBranchAddress( "weight", &weight );
@@ -158,5 +179,99 @@ void fillFromTreeAndRatio( MT2Estimate* estimate, MT2Estimate* r_effective, TTre
   }
 
   delete rprof;
+
+}
+
+
+
+void drawSingleFit( const std::string& outdir, TF1* thisFitQCD, MT2EstimateQCD* qcd, MT2EstimateQCD* all, float xMin_fit, float xMax_fit ) {
+
+
+  TH1D* thisRatioAll = all->getRatio();
+  TH1D* thisRatioQCD = qcd->getRatio();
+
+  TCanvas* c1 = new TCanvas( "c2", "", 600, 600 );
+  c1->cd();
+  c1->SetLogx();
+  c1->SetLogy();
+
+  
+  float xMin = thisRatioAll->GetXaxis()->GetXmin();
+  float xMax = thisRatioAll->GetXaxis()->GetXmax();
+
+  float yMax    = thisRatioAll->GetMaximum()*5.;
+  float yMinAll = thisRatioQCD->GetMinimum()/2.;
+  float yMin    = thisRatioQCD->GetMinimum()/2.;
+  if( yMin < 0.03 ) yMin = 0.03;
+  if( yMin > yMinAll && yMinAll>0.001 ) yMin = yMinAll;
+
+  TH2D* h2_axes = new TH2D("axes", "", 10, xMin, xMax, 10, yMin, yMax );
+  h2_axes->SetXTitle( "M_{T2} [GeV]" );
+  h2_axes->SetYTitle( "Ratio");
+  h2_axes->GetXaxis()->SetNoExponent();
+  h2_axes->GetXaxis()->SetMoreLogLabels();
+  h2_axes->GetYaxis()->SetNoExponent();
+  h2_axes->Draw();
+
+  std::vector<std::string> regionNiceNames = qcd->region->getNiceNames();
+
+  TPaveText* regionName = new TPaveText( 0.4, 0.81, 0.9, 0.9, "brNDC" );
+  regionName->SetTextAlign( 11 );
+  regionName->SetTextSize( 0.035 );
+  regionName->SetFillColor( 0 );
+  regionName->AddText( regionNiceNames[0].c_str() );
+  regionName->AddText( regionNiceNames[1].c_str() );
+  regionName->Draw("same");
+
+  TLegend* legend = new TLegend( 0.4, 0.65, 0.8, 0.82 );
+  legend->SetFillColor(0);
+  legend->SetTextSize(0.035);
+  legend->AddEntry( thisRatioAll, "MC (all)", "P" );
+  legend->AddEntry( thisRatioQCD, "MC (QCD Only)", "P" );
+  legend->AddEntry( thisFitQCD, "Fit", "L" );
+  legend->Draw("same");
+
+  TLine* lineLeft = new TLine( xMin_fit, yMin, xMin_fit, yMax );
+  lineLeft->SetLineStyle(2);
+  lineLeft->Draw("same");
+
+  TLine* lineRight = new TLine( 100., yMin, 100., yMax );
+  lineRight->SetLineStyle(2);
+  lineRight->Draw("same");
+
+  TH1D* h_band = new TH1D(Form("band_%s", thisFitQCD->GetName()) , "", 500, xMin, xMax);
+  h_band->SetMarkerSize(0);
+  h_band->SetFillColor(18); 
+  h_band->SetFillStyle(3001);
+  (TVirtualFitter::GetFitter())->GetConfidenceIntervals(h_band, 0.68);
+
+  h_band->Draw("C E3 same");
+
+  thisFitQCD->SetLineColor(46); 
+  thisFitQCD->SetLineWidth(2); 
+  thisFitQCD->Draw("L same");
+
+  thisRatioAll->SetMarkerStyle(20);
+  thisRatioAll->SetMarkerSize(1.3);
+  thisRatioAll->SetLineColor(kBlack);
+  thisRatioAll->Draw("P same");
+
+  thisRatioQCD->SetMarkerStyle(24);
+  thisRatioQCD->SetMarkerSize(1.3);
+  thisRatioQCD->SetLineColor(kBlack);
+  thisRatioQCD->Draw("P same");
+
+  gPad->RedrawAxis();
+
+  TPaveText* labelTop = MT2DrawTools::getLabelTopSimulation();
+  labelTop->Draw("same");
+
+  c1->SaveAs( Form("%s/ratio_%s.eps", outdir.c_str(), qcd->region->getName().c_str()) );
+  c1->SaveAs( Form("%s/ratio_%s.pdf", outdir.c_str(), qcd->region->getName().c_str()) );
+  
+
+  delete c1;
+  delete h2_axes;
+  delete h_band;
 
 }
