c96eccd0933858aec56dd1602e620a2f34b83419
Boris Mangano <mangano@cern.ch>
2016-06-15 16:45:15 +0200

    use compareJSON to calculate missing lumis

--- Utils/mergeRLTandGetJSONreport.sh
+++ Utils/mergeRLTandGetJSONreport.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+goldenJson=/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions16/13TeV/Cert_271036-274421_13TeV_PromptReco_Collisions16_JSON.txt
+
+echo "calculating missing lumis using json: " $goldenJson
+
 if [ $# -ne 1 ]; then
     echo "usage: merge.sh path"
     echo "e.g.: merge.sh /pnfs/psi.ch/cms/trivcat/store/user/casal/babies/MT2_CMGTools-from-CMSSW_7_4_7/prod747data_Run2015B_golden_v2/"
@@ -15,4 +19,7 @@ for i in `ls -1 $path`; do
     mkdir $prod_label/$i
     hadd $prod_label/$i/RLTInfo.root `ls $path/$i/*/0000/RLTInfo_* | awk 'BEGIN{ORS=" "}{print "dcap://t3se01.psi.ch:22125/"$1}'`
     heppy_report.py $prod_label/$i -a "" -o lumiSummary_$i.txt # this works if you have 'cmsenv' in a CMSSW release with CMG
+    compareJSON.py --sub $goldenJson $prod_label/$i/lumiSummary_$i.txt > $prod_label/$i/missingRuns_$i.txt
 done
+
+
