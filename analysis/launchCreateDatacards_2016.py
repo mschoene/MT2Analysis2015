55ba7abb3130eec1ca2d173b29b0526ae8bdaf96
Bruno <casal@cern.ch>
2016-07-15 10:41:17 +0200

    tools to launch batch datacard creation... highly non generic

---
+++ analysis/launchCreateDatacards_2016.py
@@ -0,0 +1,30 @@
+import os
+import sys
+from sys import argv
+
+if len(argv)>1:
+    model = argv[1]
+else:
+   model = "T1tttt"
+
+
+cfg = "data_Run2016_7p7ifb"
+M = range(600 if "T1" in model else 100 if "T2tt"==model else 300,2001 if "T1" in model else 1501 if "T2qq"==model else 1000,25)
+
+os.system("mkdir jobs_"+model+"_2016")
+
+for i,m in enumerate(M):
+    m1 = m
+    m2 = m1+25
+    
+    Y = range(0,m1+1,25)
+    for j,y in enumerate(Y):
+        m11=y
+        m22=Y[j+1] if j+1 < len(Y) else y+25
+
+        if m11 >= m2: 
+            continue
+        
+        command="qsub -l h_vmem=6g -q short.q -o `pwd`/jobs_"+model+"_2016/log_"+str(m1)+"_"+str(m2)+"_"+str(m11)+"_"+str(m22)+".out -e `pwd`/jobs_"+model+"_2016/log_"+str(m1)+"_"+str(m2)+"_"+str(m11)+"_"+str(m22)+".err -N creatingDatacards_"+model+"_"+str(m1)+"_"+str(m2)+"_"+str(m11)+"_"+str(m22)+" createDatacards_batch_2016.sh "+cfg+" "+model+" "+str(m1)+" "+str(m2)+" "+str(m11)+" "+str(m22)+" "
+        print command
+        os.system(command)
