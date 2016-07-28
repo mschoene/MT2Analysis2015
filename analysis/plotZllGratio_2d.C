8ef1999ec462b6238d0fc7deaea956cafcba9197
mmasciov <mario.masciovecchio@cern.ch>
2016-07-28 15:50:10 +0200

    Adding latest version of 2D Z/G ratio plotter, plus lost-lepton bin-by-bin VS standard

--- analysis/plotZllGratio_2d.C
+++ analysis/plotZllGratio_2d.C
@@ -38,26 +38,38 @@
   
   TH2D* hgammaData_jht = new TH2D("hgammaData_jht", "", nBinsHT, binsHT, nBinsJ, binsJ);
   TH2D* hgammaData_bj  = new TH2D("hgammaData_bj" , "", nBinsJ, binsJ, nBinsB, binsB);
+  hgammaData_jht->Sumw2();
+  hgammaData_bj ->Sumw2();
   
   TH2D* hgammaMC_jht   = new TH2D("hgammaMC_jht", "", nBinsHT, binsHT, nBinsJ, binsJ);
   TH2D* hgammaMC_bj    = new TH2D("hgammaMC_bj" , "", nBinsJ, binsJ, nBinsB, binsB);
+  hgammaMC_jht->Sumw2();
+  hgammaMC_bj ->Sumw2();
   
   TH2D* hgammaMCP_jht  = new TH2D("hgammaMCP_jht", "", nBinsHT, binsHT, nBinsJ, binsJ);
   TH2D* hgammaMCP_bj   = new TH2D("hgammaMCP_bj" , "", nBinsJ, binsJ, nBinsB, binsB);
+  hgammaMCP_jht->Sumw2();
+  hgammaMCP_bj ->Sumw2();
 
   TH2D* hgammaMCL_jht  = new TH2D("hgammaMCL_jht", "", nBinsHT, binsHT, nBinsJ, binsJ);
   TH2D* hgammaMCL_bj   = new TH2D("hgammaMCL_bj" , "", nBinsJ, binsJ, nBinsB, binsB);
+  hgammaMCL_jht->Sumw2();
+  hgammaMCL_bj ->Sumw2();
   
-
   TH2D* hzllData_jht = new TH2D("hzllData_jht", "", nBinsHT, binsHT, nBinsJ, binsJ);
   TH2D* hzllData_bj  = new TH2D("hzllData_bj" , "", nBinsJ, binsJ, nBinsB, binsB);
+  hzllData_jht->Sumw2();
+  hzllData_bj ->Sumw2();
   
   TH2D* hzllMC_jht   = new TH2D("hzllMC_jht", "", nBinsHT, binsHT, nBinsJ, binsJ);
   TH2D* hzllMC_bj    = new TH2D("hzllMC_bj" , "", nBinsJ, binsJ, nBinsB, binsB);
+  hzllMC_jht->Sumw2();
+  hzllMC_bj ->Sumw2();
   
   TH2D* hzllTop_jht  = new TH2D("hzllTop_jht", "", nBinsHT, binsHT, nBinsJ, binsJ);
   TH2D* hzllTop_bj   = new TH2D("hzllTop_bj" , "", nBinsJ, binsJ, nBinsB, binsB);
-
+  hzllTop_jht->Sumw2();
+  hzllTop_bj ->Sumw2();
   
   std::cout << "Projecting gamma..." << std::endl;
 
@@ -67,21 +79,26 @@
   tGmc->Project("hgammaMC_jht", "nJets:ht"    , "weight*(prompt==2 && iso<2.5  && ((ht>200 && met>200)||(ht>1000. && met>30.)) &&  ptGamma>180 && nJets>0 && mt2>200 && ht>200 )*1.23");
   tGmc->Project("hgammaMC_bj" , "nBJets:nJets", "weight*(prompt==2 && iso<2.5  && ((ht>200 && met>200)||(ht>1000. && met>30.)) &&  ptGamma>180 && nJets>0 && mt2>200 && ht>200 )*1.23");
   
-  tGmc->Project("hgammaMCP_jht", "nJets:ht"    , "weight*(prompt>0 && iso<2.5  && ((ht>200 && met>200)||(ht>1000. && met>30.)) &&  ptGamma>180 && nJets>0 && mt2>200 && ht>200 )*1.23");
-  tGmc->Project("hgammaMCP_bj" , "nBJets:nJets", "weight*(prompt>0 && iso<2.5  && ((ht>200 && met>200)||(ht>1000. && met>30.)) &&  ptGamma>180 && nJets>0 && mt2>200 && ht>200 )*1.23");
+  tGmc->Project("hgammaMCP_jht", "nJets:ht"    , "weight*(prompt>0 && id>=152 && iso<2.5  && ((ht>200 && met>200)||(ht>1000. && met>30.)) &&  ptGamma>180 && nJets>0 && mt2>200 && ht>200 )*1.23");
+  tGmc->Project("hgammaMCP_bj" , "nBJets:nJets", "weight*(prompt>0 && id>=152 && iso<2.5  && ((ht>200 && met>200)||(ht>1000. && met>30.)) &&  ptGamma>180 && nJets>0 && mt2>200 && ht>200 )*1.23");
   
-  tGmc->Project("hgammaMCL_jht", "nJets:ht"    , "weight*(iso<2.5  && ((ht>200 && met>200)||(ht>1000. && met>30.)) &&  ptGamma>180 && nJets>0 && mt2>200 && ht>200 )*1.23");
-  tGmc->Project("hgammaMCL_bj" , "nBJets:nJets", "weight*(iso<2.5  && ((ht>200 && met>200)||(ht>1000. && met>30.)) &&  ptGamma>180 && nJets>0 && mt2>200 && ht>200 )*1.23");
+  tGmc->Project("hgammaMCL_jht", "nJets:ht"    , "weight*(iso<2.5 && id>=152 && ((ht>200 && met>200)||(ht>1000. && met>30.)) &&  ptGamma>180 && nJets>0 && mt2>200 && ht>200 )*1.23");
+  tGmc->Project("hgammaMCL_bj" , "nBJets:nJets", "weight*(iso<2.5 && id>=152 && ((ht>200 && met>200)||(ht>1000. && met>30.)) &&  ptGamma>180 && nJets>0 && mt2>200 && ht>200 )*1.23");
   
   hgammaMCP_jht->Divide(hgammaMCL_jht);
   hgammaMCP_bj ->Divide(hgammaMCL_bj);
+
+  TH2D* gammaData_jht = (TH2D*) hgammaData_jht->Clone("gammaData_jht");
+  TH2D* gammaData_bj  = (TH2D*) hgammaData_bj ->Clone("gammaData_bj");
   
   hgammaData_jht->Multiply(hgammaMCP_jht);
   hgammaData_bj ->Multiply(hgammaMCP_bj);
 
   TH2D* purity_jht = (TH2D*) hgammaMCP_jht->Clone("purity_jht");
-  TH2D* purity_bj = (TH2D*) hgammaMCP_bj->Clone("purity_bj");
-
+  TH2D* purity_bj  = (TH2D*) hgammaMCP_bj ->Clone("purity_bj");
+  purity_jht->Sumw2();
+  purity_bj ->Sumw2();
+  
   hgammaData_jht->Scale(0.92);
   hgammaData_bj ->Scale(0.92);
 
@@ -100,12 +117,17 @@
   tZtop->Project("hzllTop_bj" , "nBJets:nJets", "weight*(abs(Z_mass-91.19)<10 && ((ht>200 && met>200)||(ht>1000. && met>30.)) && mt2>200 && ht>200 && nJets>0 && (Z_lepId==13 || (Z_lepId==11 && lep_tightId0>0 && lep_tightId1>0)))");
   
   TH2D* zllPurity_jht = (TH2D*) hzllMC_jht->Clone("zllPurity_jht");
+  zllPurity_jht->Sumw2();
   zllPurity_jht->Add(hzllTop_jht);
   zllPurity_jht->Divide(hzllMC_jht);  
 
   TH2D* zllPurity_bj = (TH2D*) hzllMC_bj->Clone("zllPurity_bj");
+  zllPurity_bj->Sumw2();
   zllPurity_bj->Add(hzllTop_bj);
   zllPurity_bj->Divide(hzllMC_bj);  
+
+  TH2D* zllData_jht = (TH2D*) hzllData_jht->Clone("zllData_jht");
+  TH2D* zllData_bj  = (TH2D*) hzllData_bj ->Clone("zllData_bj");
 
   hzllData_jht->Divide(zllPurity_jht);
   hzllData_bj ->Divide(zllPurity_bj );
@@ -133,10 +155,40 @@
   hzllData_jht->Divide(hzllMC_jht);
   hzllData_bj ->Divide(hzllMC_bj);
 
+  for (int x=1; x<=hzllData_jht->GetNbinsX(); ++x)
+    for (int y=1; y<=hzllData_jht->GetNbinsY(); ++y){
+
+      double error = hzllData_jht->GetBinError(x,y);
+      double value = hzllData_jht->GetBinContent(x,y);
+      
+      if (value>0) error /= value;
+      error = TMath::Sqrt(0.1*0.1+error*error);
+      error *= value;
+      
+      hzllData_jht->SetBinError(x,y, error);
+      
+    }
+    
+  for (int x=1; x<=hzllData_bj->GetNbinsX(); ++x)
+    for (int y=1; y<=hzllData_bj->GetNbinsY(); ++y){
+
+      double error = hzllData_bj->GetBinError(x,y);
+      double value = hzllData_bj->GetBinContent(x,y);
+      
+      if (value>0) error /= value;
+      error = TMath::Sqrt(0.1*0.1+error*error);
+      error *= value;
+      
+      hzllData_bj->SetBinError(x,y, error);
+      
+    }
+    
+  
   std::cout << "All ratios have been calculated. Drawing..." << std::endl;
   
   gStyle->SetOptStat(0);
   gStyle->SetPalette(51);
+  gStyle->SetPaintTextFormat("1.2f");
 
   TCanvas* cp0 = new TCanvas("cp0", "", 600, 600);
   purity_jht->SetTitle("Photon Purity");
@@ -144,7 +196,7 @@
   purity_jht->GetYaxis()->SetTitle("Jet Multiplicity");
   purity_jht->GetZaxis()->SetRangeUser(0.5, 1.0);
   purity_jht->GetZaxis()->SetLabelSize(0.02);
-  purity_jht->Draw("colz");
+  purity_jht->Draw("colz,text45E");
 
   TCanvas* cp1 = new TCanvas("cp1", "", 600, 600);
   purity_bj->SetTitle("Photon Purity");
@@ -152,26 +204,28 @@
   purity_bj->GetYaxis()->SetTitle("b-jet Multiplicity");
   purity_bj->GetZaxis()->SetRangeUser(0.5, 1.0);
   purity_bj->GetZaxis()->SetLabelSize(0.02);
-  purity_bj->Draw("colz");
+  purity_bj->Draw("colz,text45E");
 
   TCanvas* c0 = new TCanvas("c0", "", 600, 600);
   hzllData_jht->SetTitle("Z(l^{+}l^{-}) / #gamma ratio");
   hzllData_jht->GetXaxis()->SetTitle("H_{T} [GeV]");
   hzllData_jht->GetYaxis()->SetTitle("Jet Multiplicity");
   hzllData_jht->GetZaxis()->SetTitle("Data / MC");
+  hzllData_jht->SetMarkerColor(kWhite);
   hzllData_jht->GetZaxis()->SetRangeUser(0.1,1.25);
   hzllData_jht->GetZaxis()->SetTitleOffset(0.6);
   hzllData_jht->GetZaxis()->SetLabelSize(0.02);
-  hzllData_jht->Draw("colz");
+  hzllData_jht->Draw("colz,text45E");
 
   TCanvas* c1 = new TCanvas("c1", "", 600, 600);
   hzllData_bj->SetTitle("Z(l^{+}l^{-}) / #gamma ratio");
   hzllData_bj->GetXaxis()->SetTitle("Jet Multiplicity");
   hzllData_bj->GetYaxis()->SetTitle("b-jet Multiplicity");
   hzllData_bj->GetZaxis()->SetTitle("Data / MC");
+  hzllData_bj->SetMarkerColor(kWhite);
   hzllData_bj->GetZaxis()->SetRangeUser(0.1,1.25);
   hzllData_bj->GetZaxis()->SetTitleOffset(0.6);
   hzllData_bj->GetZaxis()->SetLabelSize(0.02);
-  hzllData_bj->Draw("colz");
+  hzllData_bj->Draw("colz,text45E");
 
 }
