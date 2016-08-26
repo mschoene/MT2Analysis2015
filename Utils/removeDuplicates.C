1c86a1c243b96aed987f1f4d5590b543b49b9c7e
Boris Mangano <mangano@cern.ch>
2016-08-26 18:22:33 +0200

    added script option to do file merging and overlap removal in an automatic way

--- Utils/removeDuplicates.C
+++ Utils/removeDuplicates.C
@@ -10,6 +10,8 @@ Note: you should run on each baby-tree with the option 'false' as 2nd argument t
 
 #include "TTree.h"
 #include "TFile.h"
+#include "TFileCollection.h"
+#include "TChain.h"
 
 #include <iostream>
 #include <set>
@@ -112,7 +114,94 @@ void removeDuplicates(string inputFile="duplicates.root",
   delete newfile;
 
   int stop_s=clock();
-  cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << endl;
+  cout << "elapsed time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << " seconds" << endl;
+}
 
+int removeDuplicatesFromChain(string inputFilesList,
+			      bool fillNewTree=false,
+			      string outputFile="output.root",
+			      string treeName="mt2"){
+  int start_s=clock();
+
+  // -------- Same code for TChain on remote files as in postProcessing.C
+  TChain* chain = new TChain(treeName.c_str());
+
+  //std::cout << inputFilesList.c_str() << std::endl;
+  TFileCollection *filelist = new TFileCollection("listOfFiles");
+  
+  std::cout << "Adding files list... " << std::endl;
+  filelist->AddFromFile( inputFilesList.c_str() );
+  
+  std::cout << "Adding files to chain... " << std::endl;  
+  int chainReturn = chain->AddFileInfoList((TCollection*) filelist->GetList());
+ 
+  if (chainReturn < 1) {
+    std::cout << "ERROR: input folder/fileName is not well defined. Exit!" << std::endl;
+    std::cout << "InputFileList: " << inputFilesList << std::endl;
+    return 1;
+  }
+  // ------------------------------------------------------
+
+  cout << "Chain created" << endl;
+  Long64_t nentries = chain->GetEntries();
+  
+  cout << "In input tree, nentries = " << nentries << endl;
+
+  unsigned int run,lumi;  //CMG
+  //int run,lumi;         // americans
+  unsigned long long evt;
+  chain->SetBranchAddress("run",&run);
+  chain->SetBranchAddress("lumi",&lumi);
+  chain->SetBranchAddress("evt",&evt);
+  
+  //Create a new file + a clone of old tree in new file
+  TFile *newfile = new TFile(outputFile.c_str(),"recreate");
+  TTree *newtree = chain->CloneTree(0);
+
+  cout << "Tree cloned" << endl;
+
+  //Create set where we store list of event keys
+  std::set<EventKey> previousEvents;
+
+  int nDuplicates = 0;
+
+  for (Long64_t i=0;i<nentries; i++) {
+    chain->GetEntry(i);
+
+    EventKey newEvent(run,lumi,evt);
+    bool isDuplicate = !previousEvents.insert(newEvent).second;
+
+    if(i%100000==0) {
+      time_t t = time(0);   // get time now
+      tm * now = localtime( & t );
+      cout << "Processing event: " << i << " at time " 
+	   << now->tm_hour << ":"
+	   << now->tm_min << ":"
+	   << now->tm_sec 
+	   << endl;
+    }
+    
+    if(!isDuplicate) {
+      if(fillNewTree) newtree->Fill();
+    }else{
+      nDuplicates++;
+      //cout << "Found duplicate! run,lumi,evt: " 
+      //     << run << " , " << lumi << " , " << evt <<endl;
+    }
+
+  }
+
+  cout << "Number of duplicates found: " << nDuplicates << endl;
+
+
+  //newtree->Print();
+  newtree->AutoSave();
+  delete chain;
+  delete newfile;
+
+  int stop_s=clock();
+  cout << "elapsed time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC) << " seconds" << endl;
+
+  return 0;
 }
 
